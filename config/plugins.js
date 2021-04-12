a429ad560618f2e479a3ed27db200a69c5484d75
Akshay Kumar <akshay@pop-os.localdomain>
2021-04-12 19:02:14 +0530

    added .env in repo

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'), // coder007 env('CLOUDINARY_NAME')
-        api_key: env('CLOUDINARY_KEY'), // 166624755962755 env('CLOUDINARY_KEY')
-        api_secret: env('CLOUDINARY_SECRET'), // env('CLOUDINARY_SECRET')
+        cloud_name: env('CLOUDINARY_NAME'), 
+        api_key: env('CLOUDINARY_KEY'), 
+        api_secret: env('CLOUDINARY_SECRET'), 
       },
       actionOptions: {
         upload: {},
